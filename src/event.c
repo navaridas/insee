79af40231eb001588e28ea803f7a311e944434c6
navaridas <javier.navaridas@manchester.ac.uk>
2014-06-13 17:57:01 +0100

    Update event.c
    Include <std> libraries before "local" ones, to reduce compiling issues.

--- src/event.c
+++ src/event.c
@@ -23,11 +23,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+#include <string.h>
+
 #include "globals.h"
 #include "misc.h"
 #include "event.h"
-
-#include <string.h>
 
 #if (TRACE_SUPPORT != 0)
 
